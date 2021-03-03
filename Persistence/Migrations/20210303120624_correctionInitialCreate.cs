84d880ba225271bb73c61f387051f2759f17c662
Soichiro Oda <kidd3523@icloud.com>
2021-03-03 22:14:44 +0900

    End of section 2

---
+++ Persistence/Migrations/20210303120624_correctionInitialCreate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class correctionInitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Catergory",
+                table: "Activities",
+                newName: "Category");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Activities",
+                newName: "Catergory");
+        }
+    }
+}
---
+++ Persistence/Migrations/20210303124345_SecondCorrectionInitialCreate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SecondCorrectionInitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discription",
+                table: "Activities");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discription",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
